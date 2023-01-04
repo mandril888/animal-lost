d4edcb890fcc3ff8e5848f803ce77fb98153e42e
Arturo Rubio <arubio@seocom.agency>
2023-01-04 17:53:34 +0100

    create lost card

--- src/App.vue
+++ src/App.vue
@@ -1,97 +1,43 @@
 <script setup>
-// import { RouterLink, RouterView } from "vue-router";
-// import HelloWorld from "./components/HelloWorld.vue";
-import { GoogleMap, Marker } from 'vue3-google-map';
+import { GoogleMap, InfoWindow } from 'vue3-google-map';
+import catImg from "/src/statics/cat.jpg";
 
 const center = { lat: 41.4307727, lng: 2.1745089 };
-const googleMapsApiKey = import.meta.env.GOOGLE_MAPS_API_KEY;
+const googleMapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 </script>
 
 <template>
-  <header>
-    <p style="color:white">googleMapsApiKey: {{ googleMapsApiKey }} //</p>
+  <div class="map-container">
     <GoogleMap
       :api-key="googleMapsApiKey"
       style="width: 100%; height: 500px"
       :center="center"
       :zoom="15"
     >
-      <Marker :options="{ position: center }" />
+      <InfoWindow :options="{ position: center }">
+        <div id="contet">
+          <img :src="catImg" />
+          <h1 id="firstHeading" class="firstHeading">Cat</h1>
+          <div id="bodyContent">
+            <p>Little cat lost.</p>
+            <p>Contact.</p>
+            <ul>
+              <li>Name: Arturo</li>
+              <li>Tel: 666 123 123</li>
+            </ul>
+          </div>
+        </div>
+      </InfoWindow>
     </GoogleMap>
-
-    <!-- <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div> -->
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+.map-container {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+img {
+  width: 100%;
+  max-width: 150px;
 }
 </style>
